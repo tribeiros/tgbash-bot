88732a9cac532962fbbe94c652d17c470b036945
Tiago Ribeiro <tribeiros@indracompany.com>
2018-08-03 14:53:27 -0300

    botCommands

--- app.js
+++ app.js
@@ -12,19 +12,20 @@ app.use(
 )
 
 app.post('/new-message', function(req, res) {
-  let apiTelegram = 'https://api.telegram.org'
-  let botTelegram = '651384060:AAEjQsDqPMW8MvgZpOIe9f17OVv9N4gHOuY' //tribeirosanycodeBot
-  let { message } = req.body
+  const apiTelegram = 'https://api.telegram.org'
+  const botTelegram = '651384060:AAEjQsDqPMW8MvgZpOIe9f17OVv9N4gHOuY' //tribeirosanycodeBot
+  var { message } = req.body
 //message.text = message.text.replace(/\//, "")
   var botCommands = message.text.split(" ");
+//var redirect = botCommands.indexOf(">")
+//console.log(redirect)
+  console.log(botCommands)
   console.log(`command: ${botCommands}`)
   
   // function to check bash on argument
 function checkCommand(param) {
   var containString = false;
   var arrayForbiddenCommands = ["bash", "exit", "cat", "echo","bc","crontab","alias"];
-  var containCommand = false;
-  
   
   for (s=0; s < arrayForbiddenCommands.length; s++){
     if(arrayForbiddenCommands[s].toLowerCase() == param){
@@ -34,29 +35,37 @@ function checkCommand(param) {
     }
   }
   
-  if (botCommands[0] === 'clima') {
+  if (botCommands[0] === 'clima' && botCommands[1] === undefined) {
+      checkedBash = 'missing city argument'
+      return checkedBash
+  } else if (botCommands[0] === 'clima') {
       checkedBash = shell.echo('http://wttr.in/' + botCommands[1]).stdout
       return checkedBash
   } else if (botCommands === undefined ) {
     checkedBash = 'telegram api undefined post'
     return checkedBash
   }
-
+ 
   if (containString === false){
     if (shell.exec(message.text, {silent:true}).code === 0) {
       checkedBash = shell.exec(message.text, {silent:false}).stdout
     } else {
-      checkedBash = `/bin/sh: ${message.text} not found`
+      checkedBash = shell.exec(message.text, {silent:true}).stderr
     }
   } 
   return checkedBash
 }
 
   //function result
-  const command = checkCommand(message.text)
+  var command = checkCommand(message.text)
 
 //stdout
 //console.log(command);
+  
+    if (command === ""){
+      command = 'succeeded output redirection'
+      console.log(command)
+    }
 
   axios
     .post(
@@ -67,7 +76,7 @@ function checkCommand(param) {
       }
     )
     .then(response => {
-      console.log('>>> Message posted')
+      console.log('>>> Message posted on telegram chat')
       res.end('ok')
     })
     .catch(err => {
